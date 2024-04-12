19af14d17d356464b1454144d6b097f6bd154ab9
Jomon <jomonjoyunofficial@gmail.com>
2024-04-12 11:51:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const linkButton = document.querySelectorAll(".fa-solid");
+
+linkButton.forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const getLink = e.target.parentElement.getAttribute("href");
+    const copyLinkToClipBoard = navigator.clipboard.writeText(getLink);
+
+    alert("Link has been copied to clipboard");
+  })
+})
+
+
+document.addEventListener('visibilitychange',
+function(){
+    if(document.visibilityState === "visible"){
+        document.title = "Social Links | Jomon Joy";
+        $("#favicon").attr("href","images/logo.png");
+    }
+    else {
+        document.title = "Come Back To Social Links";
+        
+    }
+});
